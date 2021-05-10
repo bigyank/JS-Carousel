3550b6d1439aedc6d53d510c3cfe9b650d0e9544
bigyank <this.is.blueorange@gmail.com>
2021-05-10 18:44:56 +0545

    carousel init

---
+++ javascript/main.js
@@ -0,0 +1,51 @@
+const slidesWraper = document.getElementById("slides-wraper");
+
+// btn
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+
+const images = slidesWraper.children;
+let counter = 1;
+const imgWidth = images[0].clientWidth;
+
+const transitionSet = "transform 0.6s ease-in-out";
+const transitionUnset = "none";
+
+// first clone
+const firstClone = images[0].cloneNode(true);
+const lastClone = images[images.length - 1].cloneNode(true);
+
+// add clones
+slidesWraper.append(firstClone);
+slidesWraper.prepend(lastClone);
+
+//  reset pic
+slidesWraper.style.transform = `translateX(${-imgWidth * 1}px)`;
+
+function moveSlide(transition) {
+  slidesWraper.style.transition = transition;
+  slidesWraper.style.transform = `translateX(${-imgWidth * counter}px)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  if (counter >= images.length - 1) return;
+  counter++;
+  moveSlide(transitionSet);
+});
+
+prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
+  counter--;
+  moveSlide(transitionSet);
+});
+
+slidesWraper.addEventListener("transitionend", () => {
+  if (counter === images.length - 1) {
+    counter = 1;
+    moveSlide(transitionUnset);
+  }
+  if (counter === 0) {
+    counter = images.length - 2;
+    moveSlide(transitionUnset);
+  }
+});
